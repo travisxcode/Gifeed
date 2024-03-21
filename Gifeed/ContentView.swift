cdc0945bf89e40aa9ad76903f0fae4075a58dfd0
Trevis Suwanwigo <s.taeshawit@gmail.com>
2024-03-21 15:34:49 -0700

    Initial Commit

---
+++ Gifeed/ContentView.swift
@@ -0,0 +1,19 @@
+// @copyright Gifeed by TrevisXcode
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gifeed/GifeedApp.swift
@@ -0,0 +1,12 @@
+// @copyright Gifeed by TrevisXcode
+
+import SwiftUI
+
+@main
+struct GifeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
