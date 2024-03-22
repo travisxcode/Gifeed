5b493535c29382a883cabf5bf9737d903044531d
Trevis Suwanwigo <s.taeshawit@gmail.com>
2024-03-21 17:48:15 -0700

    Init feed view with TCA

---
+++ Gifeed/Feed/FeedCardView.swift
@@ -0,0 +1,14 @@
+// @copyright Gifeed by TrevisXcode
+
+import SwiftUI
+import ComposableArchitecture
+
+struct FeedCardView: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+#Preview {
+  FeedCardView()
+}
---
+++ Gifeed/Feed/FeedView.swift
@@ -0,0 +1,13 @@
+// @copyright Gifeed by TrevisXcode
+
+import SwiftUI
+
+struct FeedView: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+#Preview {
+  FeedView()
+}
